5524929201004d3eff9248cc161576e58fda5e70
GarrusEn <Tinyismyson@gmail.com>
2018-07-19 20:34:23 +0500

    Task 2

---
+++ IJ2/Code/Order.cs
@@ -0,0 +1,20 @@
+﻿class Order
+{
+    public Good Good;
+    public int Count;
+
+    public int GetTotalPrice()
+    {
+        if (Good == null)
+        {
+            System.Console.WriteLine("Товар не подвезли");
+            return 0;
+        }
+        return Good.Price * Count;
+    }
+    
+    public void CompleteTheOrder()
+    {
+        Good.Count -= Count;
+    }
+}
