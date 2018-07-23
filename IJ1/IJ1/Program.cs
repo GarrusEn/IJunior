c76945f3b47e9275d7aa8e4c691c2af7abebf7eb
GarrusEn <Tinyismyson@gmail.com>
2018-07-23 22:52:39 +0500

    refactoring

--- IJ1/IJ1/Program.cs
+++ IJ1/IJ1/Program.cs
@@ -1,39 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IJ1
 {
-    class QuestionsAndAnswers
+
+    //public QuestionsAndAnswers()
+    //{
+    //    // Fill in the strings
+    //    _answers = new string[3][]
+    //    {
+    //        new string[] { "Человек", "Брандлмуха", "Кхаджит" },
+    //        new string[] { "Победить Аразота", "Стать богатым", "Найти боевых товарищей" },
+    //        new string[] { "Я отлчиный воин", "Я добротный маг", "Я могу работать в кузнице" }
+    //    };
+    //    _questions = new string[3] { "Кто вы?", "Что вы хотите?",  };
+    //    _isDoorOpen = new bool[] { false, false, false };
+
+    //    // First message
+    //    Console.WriteLine("Совершенно очевидно, что мы не берём в наш орден кого попало. По этому заполни вот эту анкету, " +
+    //                     "и мы примем решение, брать тебя или нет");
+    //}
+
+
+    class Act
     {
-        string[][] _answers;
-        string[] _questions;
-        bool[] _isDoorOpen;
+        private string _question;
+        private string[] _answers;
+        private bool _door;
 
-        public QuestionsAndAnswers()
+        public Act(string question, string[] answers)
         {
-            // Fill in the strings
-            _answers = new string[3][]
-            {
-                new string[] { "Человек", "Брандлмуха", "Кхаджит" },
-                new string[] { "Победить Аразота", "Стать богатым", "Найти боевых товарищей" },
-                new string[] { "Я отлчиный воин", "Я добротный маг", "Я могу работать в кузнице" }
-            };
-            _questions = new string[3] { "Кто вы?", "Что вы хотите?", "Чем вы можете помочь ордену?" };
-            _isDoorOpen = new bool[] { false, false, false };
-
-            // First message
-            Console.WriteLine("Совершенно очевидно, что мы не берём в наш орден кого попало. По этому заполни вот эту анкету, " +
-                             "и мы примем решение, брать тебя или нет");
+            _question = question;
+            _answers = answers;
+            _door = false;
         }
 
-        public void AskMe(int index)
+        public void AskMe()
         {
-            Console.WriteLine(_questions[index]);
-            for (int i = 0; i < _answers[index].Length; i++)
+            Console.WriteLine(_question);
+            for (int i = 0; i < _answers.Length; i++)
             {
-                Console.WriteLine("[{0}]>{1}", i, _answers[index][i]);
+                Console.WriteLine("[{0}]>{1}", i, _answers[i]);
             }
             Console.ReadLine();
-            _isDoorOpen[index] = true;
+            _door = true;
         }
     }
 
@@ -41,12 +51,20 @@ namespace IJ1
     {
         static void Main(string[] args)
         {
+            List<Act> acts = new List<Act>();
 
-            QuestionsAndAnswers act = new QuestionsAndAnswers();
+            Act act1 = new Act("Кто вы?", new string[] { "Человек", "Брандлмуха", "Кхаджит" });
+            Act act2 = new Act("Что вы хотите?", new string[] { "Победить Аразота", "Стать богатым", "Найти боевых товарищей" });
+            Act act3 = new Act("Чем вы можете помочь ордену?",
+                new string[] { "Я отлчиный воин", "Я добротный маг", "Я могу работать в кузнице" });
+
+            acts.Add(act1);
+            acts.Add(act2);
+            acts.Add(act3);        
 
             for (int i = 0; i < 3; i++)
             {
-                act.AskMe(i);
+                acts[i].AskMe();
             }
         }
     }
