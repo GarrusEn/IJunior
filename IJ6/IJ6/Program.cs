7b1f6dc641da3d1b0f86a76339007c9bcf3df05c
GarrusEn <Tinyismyson@gmail.com>
2018-08-01 00:41:42 +0500

    6,7 tasks

---
+++ IJ6/IJ6/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace IJ6
+{
+    class Stack
+    {
+        int[] Data = new int[1];
+        int Top = -1;
+        Random r = new Random();
+
+        public void ShowCakes()
+        {
+            Console.WriteLine($"На тарелке сейчас {Top+1} блинов");
+        }
+
+        public void CookCake()
+        {
+            Top++;
+            SacramentOfCreationCake();
+        }
+
+        void SacramentOfCreationCake()
+        {
+            var old = Data;
+            Data = new int[old.Length + 1];
+            Array.Copy(old, Data, old.Length);
+
+            int calories = r.Next(1, 20);
+            Console.WriteLine($"Вы испекли блинчик с {calories} калориями");
+
+            Data[Top] = calories;
+            
+        }
+        
+        public void EatCake()
+        {
+            if(Top >= 0)
+            {
+                Console.WriteLine($"В блинчике {Data[Top]} калорий");
+                Top--;
+            }
+            else
+            {
+                Console.WriteLine("Сначала надо испеч блинчик");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack pancakes = new Stack();
+            bool AreYouHungry = false;
+
+            Console.WriteLine("Вы можете использовать команды: eat // cook // stop");
+
+            while (!AreYouHungry)                
+            {
+                pancakes.ShowCakes();                
+                Console.WriteLine("Что вы хотите?");
+                string command = Console.ReadLine();
+                
+                switch (command)
+                {                    
+                    case "eat":
+                        pancakes.EatCake();
+                        break;
+                    case "cook":
+                        pancakes.CookCake();
+                        break;
+                    case "stop":
+                        AreYouHungry = true;
+                        break;
+                    default:
+                        break;
+                }
+            }            
+        }
+    }
+}
---
+++ IJ7/IJ7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace IJ7
+{
+    abstract class NPC
+    {
+        protected int Health;
+
+        public abstract void TakeDamage(int damage);
+    }
+
+    class Human : NPC
+    {
+        int agility;
+
+        public override void TakeDamage(int damage)
+        {
+            Health -= damage / agility;
+            if (Health <= 0)
+            {
+                Console.WriteLine("Я умер");
+            }
+        }
+
+        class Wombat : NPC
+        {
+            int armor;
+
+            public override void TakeDamage(int damage)
+            {
+                Health -= damage - armor;
+                if (Health <= 0)
+                {
+                    Console.WriteLine("Я умер");
+                }
+            }
+
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    
+               
+                }
+            }
+        }
+    }
+}
