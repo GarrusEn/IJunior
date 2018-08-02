2107c9e6ac578a895a7af800eeaaaf7c479ba703
GarrusEn <Tinyismyson@gmail.com>
2018-08-02 22:18:44 +0500

    8 9 10 tasks

---
+++ IJ8/IJ8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IJ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebParser parser = new WebParser("http://google.com");
+            Console.WriteLine(parser.FindContentInTag("head"));
+            Console.ReadLine();
+        }
+    }
+
+    class WebParser : WebClient
+    {
+        private string _url;
+
+        public WebParser(string url)
+        {
+            _url = url;
+        }
+
+        public string FindContentInTag(string tag)
+        {
+            string page = DownloadString(_url);
+            Regex exp = new Regex($@"(?<=\<({tag})\>)(.*)(?=\<\/({tag})\>)");
+            return exp.Match(page).Value;
+        }
+    }
+}
---
+++ IJ9,10/IJ9,10/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace IJ9_10
+{
+
+    class Player
+    {
+        public Health Health;
+        public Map Map;
+        public Vector2 Position;
+
+        public void Update()
+        {
+            if (Position.X < Map.Width)
+            {
+                Position.X++;
+                if (Map.GetCell(Position.X, Position.Y) == '#')
+                {
+                    Health.ApplyDamage(10);
+                }
+            }
+        }
+    }
+
+    class Vector2
+    {
+        public int X, Y;
+
+        public static Vector2 operator +(Vector2 vector, int i)
+        {
+            vector.X += i;
+            vector.Y += i;
+            return vector;
+        }
+
+        public static Vector2 operator -(Vector2 vector, int i)
+        {
+            vector.X -= i;
+            vector.Y -= i;
+            return vector;
+        }
+
+        public static Vector2 operator *(Vector2 vector, int i)
+        {
+            vector.X *= i;
+            vector.Y *= i;
+            return vector;
+        }
+
+        public static Vector2 operator +(Vector2 vector1, Vector2 vector2)
+        {
+            vector1.X += vector2.X;
+            vector1.Y += vector2.Y;
+            return vector1;
+        }
+
+        public static Vector2 operator -(Vector2 vector1, Vector2 vector2)
+        {
+            vector1.X -= vector2.X;
+            vector1.Y -= vector2.Y;
+            return vector1;
+        }
+
+        public static Vector2 operator *(Vector2 vector1, Vector2 vector2)
+        {
+            vector1.X *= vector2.X;
+            vector1.Y *= vector2.Y;
+            return vector1;
+        }
+    }
+
+    class Health
+    {
+        public float Value;
+
+        public bool IsAlive
+        {
+            get
+            {
+                return Value > 0;
+            }
+        }
+
+        public void ApplyDamage(float damage)
+        {
+            Value -= damage;
+        }
+    }
+
+    class Map
+    {
+        public char[,] Cells;
+
+        public char GetCell(int x, int y)
+        {
+            return Cells[x, y];
+        }
+
+        public int Width
+        {
+            get
+            {
+                return Cells.GetLength(0);
+            }
+        }
+
+        public int Heigth
+        {
+            get
+            {
+                return Cells.GetLength(1);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
